1ef4d586680eb963eb989f75ca94a6194bbafcd1
Emmanuel Suarez Acevedo <emmanueljs1@gmail.com>
2019-04-21 15:35:34 -0400

    update /events view and tests

--- spec/views/events/index.html.erb_spec.rb
+++ spec/views/events/index.html.erb_spec.rb
@@ -2,27 +2,66 @@ require 'rails_helper'
 
 RSpec.describe "events/index", type: :view do
   before(:each) do
-    assign(:events, [
-      Event.create!(
-        :title => "Event 1",
-        :description => "An Event",
-        :location => "New York City",
-        :date => DateTime.civil_from_format(:local, 2012)
-      ),
-      Event.create!(
-        :title => "Event 2",
-        :description => "An Event",
-        :location => "New York City",
-        :date => DateTime.civil_from_format(:local, 2012)
-      )
-    ])
+    @user1 = User.create(first_name: "John", last_name: "Doe", email: "john.doe@test.com", password: "password")
+    @user2 = User.create(first_name: "Jane", last_name: "Doe", email: "jane.doe@test.com", password: "password")
+    @event1 = Event.create(title: "Event 1", description: "An Event", location: "New York City", date: DateTime.civil_from_format(:local, 2012))
+    @event2 = Event.create(title: "Event 2", description: "An Event", location: "New York City", date: DateTime.civil_from_format(:local, 2012))
+    @event1.attendees << @user1 # user1 is event1's creator
+    @event2.attendees << @user2 # user2 is event2's creator
+    @events = [@event1, @event2]
+    assign(:event, @events)
+
+    controller.singleton_class.class_eval do
+      protected
+
+      helper_method :logged_in?
+      helper_method :current_user
+
+      def logged_in?
+        true
+      end
+
+      # user2
+      def current_user
+        User.last
+      end
+    end
+
+    render
   end
 
   it "renders a list of events" do
-    render
-    assert_select "tr>td", :text => "Event 1".to_s, :count => 1
-    assert_select "tr>td", :text => "Event 2".to_s, :count => 1
-    assert_select "tr>td", :text => "An Event".to_s, :count => 2
-    assert_select "tr>td", :text => "New York City".to_s, :count => 2
+    @events.each do |event|
+      assert_select "div[id=event_#{event.id}]", 1 do |div|
+        expect(div).to have_text(event.title)
+        expect(div).to have_text(event.description)
+        expect(div).to have_text(event.location)
+        expect(div).to have_text(event.creator.full_name)
+      end
+    end
+  end
+
+  it "lets the event creator delete the event" do
+    assert_select "div[id=event_#{@event2.id}]", 1 do |div|
+      expect(div).to have_text("Delete event")
+    end
+  end
+
+  it "lets the event creator edit the event" do
+    assert_select "div[id=event_#{@event2.id}]", 1 do |div|
+      expect(div).to have_text("Edit event")
+    end
+  end
+
+  it "does not let a user delete an event they didn't create" do
+    assert_select "div[id=event_#{@event1.id}]", 1 do |div|
+      expect(div).to_not have_text("Delete event")
+    end
+  end
+
+  it "does not let a user edit an event they didn't create" do
+    assert_select "div[id=event_#{@event1.id}]", 1 do |div|
+      expect(div).to_not have_text("Edit event")
+    end
   end
 end

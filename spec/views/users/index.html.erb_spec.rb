4c55d769ed19d0d84fb4aeae8b555546ad76f5cb
Emmanuel Suarez Acevedo <emmanueljs1@gmail.com>
2019-04-21 15:18:00 -0400

    update /users view and tests

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -2,27 +2,68 @@ require 'rails_helper'
 
 RSpec.describe "users/index", type: :view do
   before(:each) do
-    assign(:users, [
-      User.create!(
-        :first_name => "John",
-        :last_name => "Doe",
-        :email => "john.doe@test.com",
-        :password => "password"
-      ),
-      User.create!(
-        :first_name => "Jane",
-        :last_name => "Doe",
-        :email => "jane.doe@test.com",
-        :password => "password"
-      )
-    ])
+    @user1 = User.create(first_name: "John", last_name: "Doe", email: "john.doe@test.com", password: "password")
+    @user2 = User.create(first_name: "Jane", last_name: "Doe", email: "jane.doe@test.com", password: "password")
+    @user3 = User.create(first_name: "Jean", last_name: "Doe", email: "jean.doe@test.com", password: "password")
+    @users = [@user1, @user2, @user3]
+    assign(:users, @users)
+
+    controller.singleton_class.class_eval do
+      protected
+
+      helper_method :logged_in?
+      helper_method :current_user
+
+      def logged_in?
+        true
+      end
+
+      # user3
+      def current_user
+        User.last
+      end
+    end
+
+    render
   end
 
   it "renders a list of users" do
-    render
-    assert_select "tr>td", :text => "John Doe".to_s, :count => 1
-    assert_select "tr>td", :text => "Jane Doe".to_s, :count => 1
-    assert_select "tr>td", :text => "john.doe@test.com".to_s, :count => 1
-    assert_select "tr>td", :text => "jane.doe@test.com".to_s, :count => 1
+    @users.each do |user|
+      assert_select "div[id=user_#{user.id}]", 1 do |div|
+        expect(div).to have_text(user.full_name)
+        expect(div).to have_text(user.email)
+      end
+    end
   end
+
+  it "lets the user delete their account" do
+    assert_select "div[id=user_#{view.current_user.id}]", 1 do |div|
+      expect(div).to have_text("Delete account")
+    end
+  end
+
+  it "lets the user edit their account" do
+    assert_select "div[id=user_#{view.current_user.id}]", 1 do |div|
+      expect(div).to have_text("Edit account")
+    end
+  end
+
+  it "does not let the user delete another account" do
+    other_users = @users - [view.current_user]
+    other_users.each do |user|
+      assert_select "div[id=user_#{user.id}]", 1 do |div|
+        expect(div).to_not have_text("Delete account")
+      end
+    end
+  end
+
+  it "does not let the user edit another account" do
+    other_users = @users - [view.current_user]
+    other_users.each do |user|
+      assert_select "div[id=user_#{user.id}]", 1 do |div|
+        expect(div).to_not have_text("Edit account")
+      end
+    end
+  end
+
 end

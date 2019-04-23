accd022db4a1eb05554fc775f5e2bd89d3cbf05c
Emmanuel Suarez Acevedo <emmanueljs1@gmail.com>
2019-04-22 23:26:38 -0400

    add test for invite model (and fix bug found through test)

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,6 +2,7 @@ class Invite < ApplicationRecord
   belongs_to :inviter, class_name: 'User'
   belongs_to :invitee, class_name: 'User'
   belongs_to :event
+  validate :user_cannot_invite_self
 
   def user_cannot_invite_self
     errors.add(:inviter, 'cannot invite itself') if inviter == invitee
--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:event) { Event.create(title: 'Event', description: 'An event', location: 'New York', date: DateTime.civil_from_format(:local, 2012)) }
+  let!(:user1) { User.create(first_name: 'John', last_name: 'Doe', email: 'john.doe@test.com') }
+  let!(:user2) { User.create(first_name: 'Jane', last_name: 'Doe', email: 'jane.doe@test.com') }
+
+  describe 'validations' do
+    it 'is not valid with a user inviting themselves' do
+      invite = Invite.new(inviter: user1, invitee: user1, event: event)
+      invite.valid?
+      expect(invite.errors.messages[:inviter]).to include('cannot invite itself')
+    end
+  end
 end
